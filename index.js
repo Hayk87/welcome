e9e5aae92cc3d1aed422ebddc63a8846ff45f27a
Hayk <dallakyanhayk@gmail.com>
2020-10-23 09:58:17 +0400

    commit 4

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('<h1>Welcome!</h1>');
+	res.send(`<h1>Welcome!</h1>`);
+});
+
+app.get('/port', (req, res) => {
+	res.send(`<h1>Port: ${process.env.PORT || 3333}</h1>`);
 })
 
 app.listen(process.env.PORT || 3333);
